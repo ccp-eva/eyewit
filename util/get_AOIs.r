615705f0dd9355b77144105b4ba3d67b60c413ca
Steven Kalinke <steven.kalinke@gmail.com>
2020-04-15 23:59:02 +0200

    Unify AOI function
    AOI function takes a df and a nested aoi_collection list as input,
    and it will return by default the df along with a new AOI column.
    If the flag return_df is set to FALSE, a vector will be returned.
    
    Other changes:
    - Includes a more robust test to check df rownames/rownumbers integrety
    - Dynamic scope (you can optionally provide a nested list of ranges)

--- util/get_AOIs.r
+++ util/get_AOIs.r
@@ -1,83 +1,74 @@
-get_AOIs <- function(df, aoi_collection, scope = 0, ) {
-browser()
-  # check if rownames are sequential, if not stop execution
-  if (!is_sequence(rownames(df))) stop("The df is not in sequence. Do not remove any rows.")
+getAOIs <- function(df, aoi_collection, scope = FALSE, return_df = TRUE, coordinate_system = "ADCS") {
 
-  # destructure aoi_body
+  # check if rownames are equal to a sequence of corresponding rownumbers
+  if (!(all.equal(as.numeric(rownames(df)), 1:nrow(df)))) stop("The df is not in sequence. Do not remove any rows.")
+
+  # destructure aoi_collection
   column_name <- aoi_collection$column_name
   no_evaluation_label <- aoi_collection$no_evaluation_label
   missing_coordinate_label <- aoi_collection$missing_coordinate_label
 
+  # get df length
+  df_row_count <- nrow(df)
 
-  # get coordinate columns of df
-  x_coords <- "GazePointX..ADCSpx."
-  y_coords <- "GazePointY..ADCSpx."
-
-
-
-  # create a new AOI Column and fill it with no_evaluation_label
-  df[, column_name] <- no_evaluation_label
-  # put it to first position
-  df <- df[, c(which(colnames(df) == column_name), which(colnames(df) != column_name))]
 
   # If scope is not explicitly set, overwrite scope boundary to include all rows
-  if (length(scope) == 0) {
-    scope$start <- 1
-    scope$end <- nrow(df)
+  if (length(scope) == 1) {
+    scope <- list(start = 1, end = df_row_count)
   }
 
-  # cont vectorized version of aoi checker
-  for (i in seq_along(scope$start)) {
-    print(scope$start[i])
-    print(scope$end[i])
+  # todo check if there are multiple coordinate systems
+  # get coordinate columns of df
+  x_coords_column <- "GazePointX..ADCSpx."
+  y_coords_column <- "GazePointY..ADCSpx."
 
-    for (aoi in aoi_collection$aoilist) {
-      print(aoi)
+
+  # setup aoi vector to be filled with the given no evaluation label
+  aoi_vector <- rep(no_evaluation_label, times = df_row_count)
+
+
+  # loop over of all rows/scope ranges
+  for (seq in seq_along(scope$start)) {
+    current_start <- scope$start[seq]
+    current_end <- scope$end[seq]
+
+    # loop within a range (i.e., over single rows)
+    for (current_row in current_start:current_end) {
+
+      # loop over the aoilist
+      for (aoi in aoi_collection$aoilist) {
+        row_x_value <- df[current_row, x_coords_column]
+        row_y_value <- df[current_row, y_coords_column]
+
+        # check if either x or y is NA (or both) if so return NA
+        if (is.na(row_x_value) || is.na(row_y_value)) {
+          aoi_vector[current_row] <- missing_coordinate_label
+          break
+        }
+
+        # check the hit area
+        if (row_x_value >= aoi$x_topright    && row_y_value >= aoi$y_topright &&
+            row_x_value <= aoi$x_bottomright && row_y_value <= aoi$y_bottomright) {
+          aoi_vector[current_row] <- aoi$hit_name
+          break
+        }
+
+        # Finally, if current row’s x and y is neither NA nor in hit range assign FALSE
+        aoi_vector[current_row] <- FALSE
+      }
     }
-
   }
 
 
-
-  # AOI CHECKER
-  # checks if a pair of given coordinates belong to any AOI rectangle, and returns the AOI as string ...
-  # basically it checks for each x if its between x1 and x2, that is: |x1 <= x >= x2| same for each y
-  aoi_checker <- function(aoi_body, x_vector, y_vector) {
-
-    # if else, because if is not vectorized
-    ifelse(
-      # check for NA, if so return missing coordinate label
-      is.na(x_vector) | is.na(y_vector), return(missing_coordinate_label),
-      ifelse(
-        # check if the current cell is already occupied with a value other than FALSE
-        # ifelse(df$column_name)
-        # check for if coordinates within boundaries and return the aoi_hit_name
-        x_vector >= aoi_body$aoilist$my_first_aoi$x_topright &
-        y_vector >= aoi_body$aoilist$my_first_aoi$y_topright &
-        x_vector <= aoi_body$aoilist$my_first_aoi$x_bottomright &
-        y_vector <= aoi_body$aoilist$my_first_aoi$y_bottomright,
-        return(aoi_body$aoilist$my_first_aoi$aoi_hit_name),
-        # else return FALSE
-        FALSE # IDEA::: WRAP this in a for loop and use next HERE TO CHECK THE NEXT AOI RANGE, IF THE LAST AOI RANGE WAS HIT RETURN FALSE
-      )
-    )
+  if (return_df) {
+    # insert aoi vector into input df
+    df[, column_name] <- aoi_vector
+    # put aoi column in first position
+    df <- df[, c(which(colnames(df) == column_name), which(colnames(df) != column_name))]
+    return(df)
   }
 
-  # response <- mapply(aoi_checker,x_vector,y_vector)
-
-
-  while (length(scope) > 0) {
-    current_row_start_pos <- scope[1]
-    current_row_end_pos <- scope[2]
-    for (i in current_row_start_pos:current_row_end_pos) {
-      df[i, column_name] <- aoi_checker(df$GazePointX..ADCSpx.[i], df$GazePointY..ADCSpx.[i])
-    }
-
-    # remove current rowname pair
-    scope <- scope[!scope %in% c(current_row_start_pos, current_row_end_pos)]
-
-  }
-
-  return(df)
-
+  return(aoi_vector)
 }
+
+
