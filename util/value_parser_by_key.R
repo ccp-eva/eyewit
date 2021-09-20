9176018ed569fe1dd10ee6c7f51df3dc8b0d785d
Steven Kalinke <9831678+Kalaschnik@users.noreply.github.com>
2021-09-20 16:54:19 +0200

    Recactor destructureSting
    Making this match with:
    https://github.com/Kalaschnik/labvanced-mayo/blob/main/util/fix_labvanced_t_column.R

---
+++ util/value_parser_by_key.R
@@ -0,0 +1,41 @@
+# usage:
+
+# keys <- c("age", "sex", "location", "language")
+# delimited_string <- "36_f_leipzig_german.mp4"
+# delimiter = "_"
+# drop_nth_delimiter = 2
+# trim_right = 4
+
+# location <- value_parser_by_key(keys, delimited_string, trim_right = 4)$language # german
+# location <- value_parser_by_key(c("age"), delimited_string, drop_nth_delimiter = c(1,2,3))$age # 36fleipziggerman.mp4
+
+value_parser_by_key <- function(keys, delimited_string, delimiter = "_", drop_nth_delimiter, trim_right) {
+
+  # check argument
+  if (!missing(trim_right)) {
+    delimited_string <- str_sub(delimited_string, end = (trim_right + 1) * -1)
+  }
+
+  # check argumet
+  if (!missing(drop_nth_delimiter)) {
+
+    for (i in 1:length(drop_nth_delimiter)) {
+
+      # delimiter positions change for each iteration
+      delimiter_positions <- unlist(gregexpr(pattern = "_", delimited_string))
+
+      current_pos <- drop_nth_delimiter[i]
+
+      # check if user position is in range
+      if (!current_pos %in% seq.int(delimiter_positions)) {
+        stop(paste("There is no'", delimiter, "' at positon ", current_pos , ". Use relative indexing! E.g., 'How_are_you?' has only two underscores; refer to the first one with 1 and to the second with 2, to remove both use c(1,2)", sep = ""))
+      }
+
+      # remove delimiter at position
+      delimited_string <- sub(sprintf("^(.{%s,%s}).", delimiter_positions[current_pos] - 1, delimiter_positions[current_pos] - 1), "\\1", delimited_string)
+
+      # update drop_nth_delimiter
+      drop_nth_delimiter <- drop_nth_delimiter - 1
+    }
+  }
+}
