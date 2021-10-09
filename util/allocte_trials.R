5c0cd30195d0de4f05408bd9b339ea15743ff7e5
Steven Kalinke <9831678+Kalaschnik@users.noreply.github.com>
2021-10-09 17:56:08 +0200

    Allow to provide multiple lists of index pairs
    If multiple lists are provided, you must specify the parameter
    chunks_per_trial.
    This helpful you show for example:
    trial1-video_a.mp4
    trial1-video_b.mp4
    Yet both belong to a single trial

--- util/allocte_trials.R
+++ util/allocte_trials.R
@@ -1,16 +1,56 @@
-allocate_trials <- function(df, index_pairs, fill_StudioEventData = TRUE) {
+allocate_trials <- function(df, index_pairs, chunks_per_trial = 1, reset_to_1 = TRUE, fill_StudioEventData = TRUE) {
 
   # Check if Trial column exists, if not create it
   if (!"Trial" %in% colnames(df)) {
     df <- cbind(Trial = NA, df)
   }
 
-  trial_counter <- 1
+  if (length(index_pairs) > 2 && missing(chunks_per_trial)) {
+    stop("You provided multiple startend lists, without specifying chunks_per_trial")
+  }
+
+  if (!missing(chunks_per_trial) && length(index_pairs) > 2) {
+    bucket <- list(
+      start =
+        index_pairs[names(index_pairs) == "start"] %>%
+          unlist() %>%
+          as.integer() %>%
+          sort(),
+      end =
+        index_pairs[names(index_pairs) == "end"] %>%
+          unlist() %>%
+          as.integer() %>%
+          sort()
+    )
+
+    # clean index_pairs, since it contains multiple starts/ends
+    rm(index_pairs)
+    index_pairs <- bucket
+  }
+
+  trial_counter <- 0
+
+  # if reset_to_1 == False, look for max number in Trials
+  if (!reset_to_1) {
+    trial_counter <- max(df$Trial, na.rm = TRUE)
+  }
 
   for (i in seq_along(index_pairs$start)) {
-
     start_pos <- index_pairs$start[i]
     end_pos <- index_pairs$end[i]
+
+    # increment trial counter
+    if (!missing(chunks_per_trial)) {
+      # check if i modulo chunks_per_trial is 1, if so increment, examples:
+      #   1 %% 2 = 1 -> increment
+      #   2 %% 2 = 0 -> dont increment (next chunk will also be 1)
+      #   3 %% 2 = 1 -> increment, etc...
+      if (i %% chunks_per_trial == 1) {
+        trial_counter <- trial_counter + 1
+      }
+    } else {
+      trial_counter <- trial_counter + 1
+    }
 
     # assign current trial counter to inter trial chunk
     df$Trial[start_pos:end_pos] <- rep(trial_counter, length(start_pos:end_pos))
@@ -19,10 +59,7 @@ allocate_trials <- function(df, index_pairs, fill_StudioEventData = TRUE) {
     # Fill up StudioEventData
     if (fill_StudioEventData) df$StudioEventData[start_pos:end_pos] <- df$StudioEventDat[start_pos]
 
-    # increment trial counter
-    trial_counter <- trial_counter + 1
   }
 
   return(df)
-
 }
