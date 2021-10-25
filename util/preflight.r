8a9d7a93a1e1b339470f0967d7758cb6e4c383bb
Steven Kalinke <9831678+Kalaschnik@users.noreply.github.com>
2021-10-25 21:20:17 +0200

    Allow new interface structure

--- util/preflight.r
+++ util/preflight.r
@@ -1,15 +1,21 @@
-preflight <- function(df, cols) {
+preflight <- function(df, interface) {
 
+  # check if a mandatory columns is missing
+  if (FALSE %in% (interface$mc %in% names(df))) {
+    stop(paste('The following column(s) are missing:', cols[which(cols %in% names(df) == FALSE)]))
+  }
+
+  # create a lean df (only mandatory columns)
+  df <- df[, c(interface$mc, interface$coi)]
 
   # remove spaces, brackets, periods in column names
   names(df) <- gsub("\\s+", "", names(df))
   names(df) <- gsub("\\(|\\)", "", names(df))
   names(df) <- gsub("\\.", "", names(df))
 
-  # check if a mandatory columns is missing
-  if (FALSE %in% (cols %in% names(df))) {
-    stop(paste('The following column(s) are missing:', cols[which(cols %in% names(df) == FALSE)]))
-  }
+  # renaming x and y coords columns to x/y (tidyverse: df <- rename(df, x = GazePointXADCSpx))
+  names(df)[names(df) == interface$xy_columns$x] <- 'x'
+  names(df)[names(df) == interface$xy_columns$y] <- 'y'
 
   # check if rownames are equal to a sequence of corresponding rownumbers
   if (!isTRUE((all.equal(as.numeric(rownames(df)), 1:nrow(df))))) {
@@ -17,6 +23,7 @@ preflight <- function(df, cols) {
   }
 
   # check if aoi sets have no overlapping coordinates in a single set
+  # https://github.com/Kalaschnik/tobii-eyetracking-utilities/issues/25
   # todo
   return(df)
 }
