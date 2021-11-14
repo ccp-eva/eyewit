0094cee79e38126d54b8ba989f53ac02ebe10fad
Steven Kalinke <9831678+Kalaschnik@users.noreply.github.com>
2021-11-14 20:08:06 +0100

    Add get_lookaway_scope_end function
    This function returns an updated scope$end vector for a given, df, scope,
    and lookaway_stop time criterion in ms.
    
    Whenever a non-fixation event is greater or equal the provided lookaway_stop,
    the scope_end will be updated the prior fixation_index.
    
    For example;
    This comes in handy if you want to stop calculating looking durations for
    subjects when they look away from the screen for the provided time in ms.

---
+++ util/get_lookaway_scope_end.R
@@ -0,0 +1,30 @@
+get_lookaway_scope_end <- function(df, scope, lookaway_stop) {
+
+  # destructure provided scope
+  scope_end <- scope$end
+
+  # get non-fixation data
+  nf_data <- get_non_fixation_data(df, scope)
+
+  # loop over durations within nf_data to check which durations are greater lookaway_stop
+  for (i in 1:length(nf_data)) {
+
+    # check if lookaway_stop criterion is fulfilled
+    if (TRUE %in% (nf_data[[i]]$durations > lookaway_stop)) {
+
+      # get the index of the very first element being greater equal the lookaway_stop
+      # ... as all subsequent elements great equal the lookaway_stop must be ignored
+
+      # get the duration index that matches the criterion
+      lookaway_index <- min(which(nf_data[[i]]$durations >= lookaway_stop))
+
+      # overwrite scope_end pos (subtract 1 to get the end of the last
+      # ... fixation satisfying time criterion)
+      scope_end[i] <- nf_data[[i]]$start[lookaway_index] - 1
+    }
+  }
+
+  return(scope_end)
+}
+
+
