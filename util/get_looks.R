5307b01c735dd0e6b00f0ca1d7293e1d66e825b6
Steven Kalinke <9831678+Kalaschnik@users.noreply.github.com>
2021-11-30 11:05:40 +0100

    Skip if fixation index contains multiple hit_names within the same fixation

--- util/get_looks.R
+++ util/get_looks.R
@@ -212,15 +212,21 @@ get_looks <- function(
       # see reference for the check below: (https://stackoverflow.com/a/2191824/2258480)
       # This should not happen because we have a preflight check for overlapping AOIs, yet this might should stay in anyway
       if (sum(hit_names %in% hit_names_in_FixationIndex, na.rm = TRUE) > 1) {
+
         # log this index
         bad_fixation_indexes <- c(bad_fixation_indexes, i)
-        # stop execution, as this should be impossible
-        stop()
+        # stop execution, as this should be impossible (but it is, it is a tobii thing)
+        # stop()
         # go to next index
-        # next
+        warning(str_interp("At Fixation index: ${i}, are multiple hit names (aois) within the same Fixation (see bad_fixation_indexes"))
+        next
       }
 
-      # ----- Looking frequency (Looks) & Gaze Shifts -----
+      # FYI: outside label can share the the same fi with a hitname
+      # do not try to prevent that!
+      # see: https://github.com/Kalaschnik/media/possible-overlapping-aoi-with-outside-label.png
+
+      # ----- Looking frequency (Looks), Gaze Shifts, Last Hit Name -----
       # Check if there is a defined hit_name within all the hit_names of the current fixation index
       if (TRUE %in% (hit_names %in% hit_names_in_FixationIndex)) {
         # get the current hit_name within the current fixation index
