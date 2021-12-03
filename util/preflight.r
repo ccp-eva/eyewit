1498840e4981ff5a749f05f199884e4fafa0089c
Steven Kalinke <9831678+Kalaschnik@users.noreply.github.com>
2021-12-03 14:30:53 +0100

    Message does not need \n

--- util/preflight.r
+++ util/preflight.r
@@ -1,28 +1,28 @@
 preflight <- function(df, interface) {
 
-  message("====================================================================\n")
-  message("=====================   🧾  PREFLIGHT REPORT   =====================\n")
-  message("====================================================================\n")
+  message("====================================================================")
+  message("=====================   🧾  PREFLIGHT REPORT   =====================")
+  message("====================================================================")
 
   # check if a mandatory columns is missing
   if (FALSE %in% (interface$mc %in% names(df))) {
     stop(paste('The following column(s) are missing:', cols[which(cols %in% names(df) == FALSE)]))
   }
 
-  message("   📊  All mandatory columns found\n")
+  message("   📊  All mandatory columns found")
 
 
   # create a lean df (only mandatory columns)
   df <- df[, c(interface$mc, interface$coi)]
 
-  message("   ⚖️  Created a lean df\n")
+  message("   ⚖️  Created a lean df")
 
 
   # renaming x and y coords columns to x/y (tidyverse: df <- rename(df, x = GazePointXADCSpx))
   names(df)[names(df) == interface$xy_columns$x] <- 'x'
   names(df)[names(df) == interface$xy_columns$y] <- 'y'
 
-  message("   🗺️  Renaming coordinate columns to x and y\n")
+  message("   🗺️  Renaming coordinate columns to x and y")
 
 
   # remove spaces, brackets, periods in column names
@@ -30,7 +30,7 @@ preflight <- function(df, interface) {
   names(df) <- gsub("\\(|\\)", "", names(df))
   names(df) <- gsub("\\.", "", names(df))
 
-  message("   💅  Column names fixed (no spaces, brackets, periods)\n")
+  message("   💅  Column names fixed (no spaces, brackets, periods)")
 
 
   # check if rownames are equal to a sequence of corresponding rownumbers
@@ -38,16 +38,16 @@ preflight <- function(df, interface) {
     stop("The df is not a incremental sequence. Do not remove any rows.")
   }
 
-  message("   🔢  Dataframe is in incremental sequence\n")
+  message("   🔢  Dataframe is in incremental sequence")
 
   # check if aoi sets have no overlapping coordinates in a single set
   is_aoilist_overlapping(interface$aoisets)
 
-  message("   ⚔️  No intersecting AOIs found\n")
-
+  message("   ⚔️  No intersecting AOIs found")
 
   cat("--------------------------------------------------------------------\n")
   message("--------------------------------------------------------------------\n")
+  message("--------------------------------------------------------------------")
 
   return(df)
 }
