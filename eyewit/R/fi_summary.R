027ddcbf9218afa369108b56f260962805bb1d06
Steven Kalinke <9831678+Kalaschnik@users.noreply.github.com>
2022-03-08 13:16:10 +0100

    Rename column names, add trimmed function, and fix range cut

--- eyewit/R/fi_summary.R
+++ eyewit/R/fi_summary.R
@@ -17,7 +17,7 @@
 #
 #' ```
 #' tribble(
-#'   ~FI, ~RowStart, ~RowEnd, ~AOIColA,             ~AOIColN,
+#'   ~FI, ~FIrnS, ~FIrnE, ~AOIColA,             ~AOIColN,
 #'   1,   15,        30,      c("left"),            c("onscreen"),
 #'   2,   45,        70,      c("right"),           c("onscreen"),
 #'   3,   80,        99,      c("left", "outside"), c("onscreen"),
@@ -32,7 +32,7 @@
 #'
 #' ```
 #' tribble(
-#'   ~FI, ~RowStart, ~RowEnd, ~AOIColA,             ~AOIColAClean, ~AOIColN,      ~AOIColNClean,
+#'   ~FI, ~FIrnS, ~FIrnE, ~AOIColA,             ~AOIColAClean, ~AOIColN,      ~AOIColNClean,
 #'   1,   15,        30,      c("left"),            c("left"),     c("onscreen"), NA,
 #'   2,   45,        70,      c("right"),           c("right"),    c("onscreen"), NA,
 #'   3,   80,        99,      c("left", "outside"), c("left"),     c("onscreen"), NA,
@@ -40,17 +40,17 @@
 #' ```
 #'
 #'
-#' ## df, aoisets, trial scope
-#' If you provide a trial scopes, the returned df will be trimmed down to respect the row ranges. It
+#' ## df, aoisets, trial range
+#' If you provide a trial range, the returned df will be trimmed down to respect the row ranges. It
 #' will use have an additional column `FGapDurTrlEnd` which is similar to `FGapDur`; yet, this
-#' column will respect the trial scope end in the calculation for the duration. For example,
+#' column will respect the trial range end in the calculation for the duration. For example,
 #' if the fixation index 420 is the last one in trail 7, `FGapDur` will show the time difference
 #' from 420 to 421. In contrast, `FGapDurTrlEnd` will show the time difference from 420 to the
-#' scope end time (e.g., "MovieEnd"). Thus, `FGapDurTrlEnd` is naturally smaller than `FGapDur`.
+#' trialrng end time (e.g., "MovieEnd"). Thus, `FGapDurTrlEnd` is naturally smaller than `FGapDur`.
 #'
 #' @param df A dataframe containing columns created by [get_aois].
 #' @param aoi_sets *[Optional]* A list of AOI sets.
-#' @param scope *[Optional]* A trial scope list.
+#' @param trialrange *[Optional]* A trial range (trlr) list.
 #' @param show_non_hn_labels *[Optional]* TRUE or FALSE (default).
 #'
 #' @return Returns a dataframe (tibble) providing AOI information for all fixation indexes
@@ -63,22 +63,22 @@
 #' }
 fi_summary <- function(df,
                        aoisets = NA,
-                       scope = NA,
+                       trialrange = NA,
                        show_non_hn_labels = FALSE) {
 
   # create tibble based on fi2rn() and map it in a df
   fi_df <- tibble::tibble(.rows = length(.eyewit_utils$fi2rn$fistart))
   fi_df$FI <- seq.int(.eyewit_utils$fi2rn$fistart)
-  fi_df$RowStart <- .eyewit_utils$fi2rn$fistart
-  fi_df$RowEnd <- .eyewit_utils$fi2rn$fiend
+  fi_df$FIrnS <- .eyewit_utils$fi2rn$fistart
+  fi_df$FIrnE <- .eyewit_utils$fi2rn$fiend
 
 
-  # if scope and aoisets are missing, return here
-  if (missing(scope) && missing(aoisets)) {
+  # if trialrange and aoisets are missing, return here
+  if (missing(trialrange) && missing(aoisets)) {
     return(fi_df)
   }
 
-  # if aoisets are set but scope is missing, continue here
+  # if aoisets are set but trialrange is missing, continue here
   if (!missing(aoisets)) {
     for (i in seq.int(aoisets)) {
       current_colname <- aoisets[[i]]$column_name
@@ -93,9 +93,9 @@ fi_summary <- function(df,
       fi_df$Trial <- vector("integer", nrow(fi_df))
 
       # add columns for time tracking
-      fi_df$TSStart <- vector("integer", nrow(fi_df))
-      fi_df$TSEnd <- vector("integer", nrow(fi_df))
-      fi_df$TSEnd1 <- vector("integer", nrow(fi_df)) # The sample after the fi end
+      fi_df$FIrtsS <- vector("integer", nrow(fi_df)) # Fixation Index Recording Timestamp Start
+      fi_df$FIrtsE <- vector("integer", nrow(fi_df)) # Fixation Index Recording Timestamp End
+      fi_df$FIrtsE1 <- vector("integer", nrow(fi_df)) # The sample after FIrtsE
       fi_df$FDur <- vector("integer", nrow(fi_df))
       fi_df$FGapDur <- vector("integer", nrow(fi_df))
 
@@ -107,17 +107,17 @@ fi_summary <- function(df,
           df[[current_colname]][which(df$FixationIndex == fi)] |>
           unique()
 
-        fi_df$TSStart[fi] <- df$RecordingTimestamp[which(df$FixationIndex == fi)] |> min()
-        fi_df$TSEnd[fi] <- df$RecordingTimestamp[which(df$FixationIndex == fi)] |> max()
-        # for end value use the i + 1 sample from the TSEnd to get correct durations (compare with
+        fi_df$FIrtsS[fi] <- df$RecordingTimestamp[which(df$FixationIndex == fi)] |> min()
+        fi_df$FIrtsE[fi] <- df$RecordingTimestamp[which(df$FixationIndex == fi)] |> max()
+        # for end value use the i + 1 sample from the FIrtsE to get correct durations (compare with
         # ... GazeEventDuration column)
-        fi_df$TSEnd1[fi] <- df$RecordingTimestamp[which(df$FixationIndex == fi) |> max() + 1]
-        fi_df$FDur[fi] <- fi_df$TSEnd1[fi] - fi_df$TSStart[fi]
+        fi_df$FIrtsE1[fi] <- df$RecordingTimestamp[which(df$FixationIndex == fi) |> max() + 1]
+        fi_df$FDur[fi] <- fi_df$FIrtsE1[fi] - fi_df$FIrtsS[fi]
 
 
         # Calculate gap duration between fixation indexes
-        ending_times_of_former_fixation <- fi_df$TSEnd1[1:length(fi_df$TSEnd1) - 1]
-        starting_times_of_latter_fixation <- fi_df$TSStart[2:length(fi_df$TSStart)]
+        ending_times_of_former_fixation <- fi_df$FIrtsE1[1:length(fi_df$FIrtsE1) - 1]
+        starting_times_of_latter_fixation <- fi_df$FIrtsS[2:length(fi_df$FIrtsS)]
         fixation_gap_duration <- starting_times_of_latter_fixation - ending_times_of_former_fixation
         fi_df$FGapDur <- c(fixation_gap_duration, NA)
 
@@ -134,8 +134,10 @@ fi_summary <- function(df,
           fi_df$Trial[fi] <- NA
           next
         }
+
         fi_df$Trial[fi] <- df$Trial[which(df$FixationIndex == fi)] |> min(na.rm = TRUE)
       }
+
 
       # attach current_hitnames list to fi_df
       fi_df[[current_colname]] <- current_hitnames
@@ -156,67 +158,98 @@ fi_summary <- function(df,
         # remove meta labels for outside, no fixation data, and no eval range
         # specifically remove all entries that are not legit_hitnames
         for (fi in seq.int(nrow(fi_df))) {
-          fi_df[[clean_colname]][[fi]] <- legit_hitnames[which(legit_hitnames %in% fi_df$AOIActionPhaseBody[[fi]])]
+          fi_df[[clean_colname]][[fi]] <-
+            legit_hitnames[which(legit_hitnames %in% fi_df$AOIActionPhaseBody[[fi]])]
         }
 
         # check for character 0 and replace with NA (https://stackoverflow.com/a/44766446/2258480)
         fi_df[[clean_colname]] <-
-          lapply(fi_df$AOIActionPhaseBodyClean, function(x) if (identical(x, character(0))) NA_character_ else x)
+          lapply(fi_df$AOIActionPhaseBodyClean,
+                 function(x) if (identical(x, character(0))) NA_character_ else x)
       }
     }
   }
 
-  if (!missing(scope)) {
+  if (!missing(trialrange)) {
 
-    # add fixation gap duration column which is sensitive to scope end
+    # add fixation gap duration column which is sensitive to trialrange end
     # init it with the contents fo FGapDur
     fi_df <- tibble::add_column(fi_df, FGapDurTrlEnd = fi_df$FGapDur, .after = "FGapDur")
+
+    # add fixation duration column which is sensitive to beginning and endings of trial ranges
+    # That is overlapping fixation indexes are trimmed to respect the trial range.
+    # This is identical to get_looks’ intra_scope_cut
+    fi_df <- tibble::add_column(fi_df, FDurTrlTrimmed = fi_df$FDur, .after = "FDur")
 
     # prepare subsetting df
     fi_df_sub <- tibble::tibble()
 
-    for (trial in seq.int(scope$start)) {
-      current_scope_start <- scope$start[trial]
-      current_scope_end <- scope$end[trial]
+    for (trial in seq.int(trialrange$start)) {
 
-      subset_start <- which(fi_df$RowStart <= current_scope_start) |> max()
-      # it may be that there no more fi_df$RowEnd indexes which are greater than curren_scope_end
+      current_trialrange_start <- trialrange$start[trial]
+      current_trialrange_end <- trialrange$end[trial]
+
+      subset_start <- which(fi_df$FIrnS <= current_trialrange_start) |> max()
+      # it may be that there no more fi_df$FIrnE indexes greater than curren_trialrange_end
       # ... especially in the last trial. Thus, we use the last Fixation Index
       subset_end <- ifelse(
-        any(fi_df$RowEnd >= current_scope_end),
-        which(fi_df$RowEnd >= current_scope_end) |> min(),
+        any(fi_df$FIrnE >= current_trialrange_end),
+        which(fi_df$FIrnE >= current_trialrange_end) |> min(),
         fi_df$FI |> max()
       )
 
-      # the current_scope_start must be within:
-      # ... fi_df$RowStart[subset_start] and fi_df$RowEnd[subset_start]
-      # if that is not the case increment fi by to get an accurate subset
-      if (
-        !(fi_df$RowStart[subset_start] <= current_scope_start &&
-          current_scope_start <= fi_df$RowEnd[subset_start])
-      ) {
+      # the current_trialrange_start must be smaller/larger compared to fi_df$FIrnE/S
+      # ... this makes sure to get the correct trial range for subsetting!
+      if (!(current_trialrange_start <= fi_df$FIrnE[subset_start])) {
         subset_start <- subset_start + 1L
       }
 
-      # same logic for current_scope_end...
-      if (
-        !(fi_df$RowStart[subset_end] <= current_scope_end &&
-          current_scope_end <= fi_df$RowEnd[subset_end])
-      ) {
+      # same logic for current_trialrange_end...
+      if (!(fi_df$FIrnS[subset_end] <= current_trialrange_end)) {
         subset_end <- subset_end - 1L
       }
 
 
       # overwrite fixation gap duration trial end for last entry
-      fi_df$FGapDurTrlEnd[subset_end] <- df$RecordingTimestamp[current_scope_end] - fi_df$TSEnd1[subset_end]
-
+      fi_df$FGapDurTrlEnd[subset_end] <-
+        df$RecordingTimestamp[current_trialrange_end] - fi_df$FIrtsE1[subset_end]
 
       # subset
       fi_df_sub <- rbind(fi_df_sub, fi_df[subset_start:subset_end, ])
+
+    } # loop end
+
+
+    # overwrite fi_df with fi_df_sub if trialrange was set
+    fi_df <- fi_df_sub
+
+
+    # check if there are falsy leading and trailing durations
+    trimmed_durations <- get_trimmed_fixation_durations(df, trialrange)
+    if (FALSE %in% is.na(trimmed_durations$leading_durations)) {
+      # iterate over leading and trailing positions and replace corresponding values in FDurTrlTrimmed
+      for (trialpos in trimmed_durations$leading_trial_positions) {
+        fi_df$FDurTrlTrimmed[which(fi_df$Trial == trialpos) |> min()] <-
+          trimmed_durations$leading_durations[trialpos]
+      }
+
+      for (trialpos in trimmed_durations$trailing_trial_positions) {
+        fi_df$FDurTrlTrimmed[which(fi_df$Trial == trialpos) |> max()] <-
+          trimmed_durations$trailing_durations[trialpos]
+      }
     }
 
-    # overwrite fi_df with fi_df_sub if scope was set
-    fi_df <- fi_df_sub
+
+    # Add Columns for Trial rn Start/End for comparison with FIrnS/E
+    fi_df <- tibble::add_column(fi_df, TrlrnS = fi_df$Trial, .after = "Trial")
+    fi_df <- tibble::add_column(fi_df, TrlrnE = fi_df$Trial, .after = "TrlrnS")
+    # fill values based on provided trialrange
+    fi_df$TrlrnS <- trialrange$start[fi_df$TrlrnS]
+    fi_df$TrlrnE <- trialrange$end[fi_df$TrlrnE]
+
+    # Add trial recording timestamps for reference
+    fi_df <- tibble::add_column(fi_df, TrlrtsS = df$RecordingTimestamp[fi_df$TrlrnS], .after = "TrlrnE")
+    fi_df <- tibble::add_column(fi_df, TrlrtsE = df$RecordingTimestamp[fi_df$TrlrnE], .after = "TrlrtsS")
   }
 
   return(fi_df)
