74cecab7921712b539f7468a33fd518282f739cf
Steven Kalinke <9831678+Kalaschnik@users.noreply.github.com>
2022-11-09 10:51:04 +0100

    Add interface for different vendor column names

---
+++ eyewit/R/vendor_check.R
@@ -0,0 +1,36 @@
+#' vendor_check
+#'
+#' @param df Your raw dataframe
+#'
+#' @return Returns a `vendor_lookup` key as string
+#' @export
+#'
+vendor_check <- function(df) {
+	p_color <- crayon::make_style("white")
+	p_weight <- crayon::make_style("bold")
+	p_bg <- crayon::make_style(rgb(0, 0.42, 0.40), bg = TRUE)
+	p_h1 <- crayon::combine_styles(p_color, p_weight, p_bg)
+	p_text <- crayon::combine_styles(p_color, p_bg)
+	cat(p_h1("\tVendor/Software: "))
+
+	# extract x/y coordinate labels from all vendors, which will serve as an id for now
+	vendor_x <- c()
+	vendor_y <- c()
+	for (i in vendor_lookup) {
+		vendor_x <- c(vendor_x, i$x)
+		vendor_y <- c(vendor_y, i$y)
+	}
+
+	target_vendor_x <- which(vendor_x %in% names(df))
+	target_vendor_y <- which(vendor_y %in% names(df))
+
+	# check if there is one and only one string match within the column names
+	stopifnot(length(target_vendor_x) == 1)
+
+	# target_vendor_x and target_vendor_y must share the same TRUE index
+	stopifnot(target_vendor_x == target_vendor_y)
+
+	return(names(vendor_lookup)[target_vendor_x])
+}
+
+
---
+++ eyewit/R/vendor_lookup.R
@@ -0,0 +1,39 @@
+# make sure that all keynames are the same for every vendor
+# keynames need to match with the names in preflight
+# set keys values that don't match a any column to NULL
+vendor_lookup <- list(
+	tobii_studio = list(
+		participant = "ParticipantName",							# id1234
+		timestamp = "RecordingTimestamp",							# 11, 28, 45 (equidistant values, sound never contain NA)
+		event = "StudioEvent",												# MovieStart, MovieEnd, NA, ... (Markers)
+		eventValue = "StudioEventData",								# 2_b_d2.mp4 (stimuli being displayed)
+		stimulusName = NULL,
+		mediaName = NULL,
+		gazeType = "GazeEventType",										# Unclassified, Fixation, Saccade, ...
+		fi = "FixationIndex",													# 1, 2, 3, NA, 4, 5 ...
+		gazeDuration = "GazeEventDuration",						# 167, 50, 83, NA
+		x = "GazePointX (ADCSpx)",										# 806, NA, ...
+		fix = NULL,
+		y = "GazePointY (ADCSpx)",										# 234, NA, ...
+		fiy = NULL,
+		pupilDiameterLeft = NULL,
+		pupilDiameterRight = NULL
+	),
+	tobii_prolab = list(
+		participant = "Participant name",							# id1234
+		timestamp = "Recording timestamp",						# 79057, 79057, ...
+		event = "Event",															# VideoStimulusStart, VideoStimulusEnd, NA, Keyboard, MouseEvent,
+		eventValue = "Event value",										# ImageStimulusStart -> ball_center, Keyboard -> Space, MouseEvent -> Up, Left
+		stimulusName = "Presented Stimulus name",			# ball_center
+		mediaName = "Presented Media name",						# ball_center.jpg
+		gazeType = "Eye movement type",								# Fixation, Saccade, Unclassified, NA, EyesNotFound
+		fi = "eye movement type index",								# Fixation -> 82, 83, Saccade -> 104, Fixation 84 ...
+		gazeDuration = "Gaze event duration",					# 168, 842, ...
+		x = "Gaze point X",														# 800, 742, ... Raw gaze coordinates
+		fix = "Fixation point X",											# 803, 740, ...Horizontal coordinate of the avg gaze point for both eyes
+		y = "Gaze point Y",														#
+		fiy = "Fixation point Y",											#
+		pupilDiameterLeft = "Pupil diameter left",		# 3552
+		pupilDiameterRight = "Pupil diameter right"		# 3527
+	)
+)
